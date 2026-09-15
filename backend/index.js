fe7994d4c2bd9639ce132c623ceab9decae642cf
ydvrahul160-netizen <ydvrahul160@gmail.com>
2026-09-15 16:29:21 +0530

    feat: configure CORS, cookies and auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 // Import required packages
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Import database connection function
 import connectDb from "./config/db.js";
 
-// Load environment variables from .env file
-dotenv.config();
+// Import routes
+import authRouter from "./routes/auth.routes.js";
+
+
 
 // Create Express application
 const app = express();
@@ -15,6 +22,28 @@ const app = express();
 // Use 5000 as fallback if PORT is not defined
 const port = process.env.PORT || 5000;
 
+// ---------------------------------------- Middleware --------------------------------------------
+
+// Allow frontend to communicate with backend
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+// Parse JSON data from requests
+app.use(express.json());
+
+// Read cookies from incoming requests
+app.use(cookieParser());
+
+// ------------------------------------------- Routes ----------------------------------------------
+// Authentication routes
+app.use("/api/auth", authRouter);
+
+
+// ----------------------------------------- Start Server ------------------------------------------
 // Start the server
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
