9dacb5bf51e449e087defbbbb164e85b40e7d221
ydvrahul160-netizen <ydvrahul160@gmail.com>
2026-09-15 15:15:16 +0530

    feat: add user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,31 +1,41 @@
 import mongoose from "mongoose";
 
+// Define user schema
 const userSchema = new mongoose.Schema(
   {
     fullName: {
       type: String,
       required: true,
     },
+
     email: {
       type: String,
       required: true,
       unique: true,
     },
+
     password: {
       type: String,
     },
+
     mobile: {
       type: String,
       required: true,
     },
+
+    // Define user roles
     role: {
       type: String,
       enum: ["user", "owner", "deliveryBoy"],
       required: true,
     },
   },
+
+  // track user create time
   { timestamps: true },
 );
 
+// Create User model
 const User = mongoose.model("User", userSchema);
+
 export default User;
