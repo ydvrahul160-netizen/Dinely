8fd4668021d18a83ed076b29610ad0913eb38ed4
ydvrahul160-netizen <ydvrahul160@gmail.com>
2026-09-15 15:54:56 +0530

    feat: implement user sign-in authentication

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -3,6 +3,8 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import gentoken from "../utils/token.js";
 
+
+//------------------------------------ SIGN UP CONTROLLER --------------------------------------------
 export const signUp = async (req, res) => {
   try {
     const { fullName, email, password, mobile, role } = req.body;
@@ -42,5 +44,35 @@ export const signUp = async (req, res) => {
     return res.status(201).json(user);
   } catch (error) {
     return res.status(500).json(`sign up error ${error}`);
+  }
+};
+
+//------------------------------------ SIGN IN CONTROLLER --------------------------------------------
+export const signIn = async (req, res) => {
+  try {
+    const {  email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+   
+    const token = await gentoken(User._id);
+
+    response.cookie("token", token, {
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(`sign in error ${error}`);
   }
 };
