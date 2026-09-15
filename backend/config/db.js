58ced24f4f570fa79c540d78c3600ff9f2eb081d
ydvrahul160-netizen <ydvrahul160@gmail.com>
2026-09-15 15:12:45 +0530

    feat: connect application to MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,18 @@
+// Import Mongoose for MongoDB database connection
 import mongoose from "mongoose";
 
+// Create function to connect with MongoDB
 const connectDb = async () => {
   try {
+    // Connect to MongoDB using the URL from environment variables
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log(`DB connected`);
+
+    console.log("DB connected");
   } catch (error) {
+    // Handle database connection errors
     console.log("DB connection error", error.message);
   }
 };
 
+// Export database connection function
 export default connectDb;
